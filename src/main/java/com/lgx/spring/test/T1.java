15664c351d37ce428415d2fd41606226469ed3f6
lgx <2029666191@qq.com>
2022-01-06 10:54:04 +0800

    test5

--- src/main/java/com/lgx/spring/test/T1.java
+++ src/main/java/com/lgx/spring/test/T1.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.BeanFactory;
 public abstract class T1 implements BeanFactory {
 
 	public static void main(String[] args) {
-		System.out.println(333);
+		System.out.println(555);
 	}
 
 }
