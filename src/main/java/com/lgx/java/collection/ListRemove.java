047693eecc339f15522ccf5fbc2602e68a7ed3ec
richard <richard.li@biposervice.com>
2022-10-12 16:30:39 +0800

    list delete

---
+++ src/main/java/com/lgx/java/collection/ListRemove.java
@@ -0,0 +1,42 @@
+package com.lgx.java.collection;
+
+import org.apache.commons.compress.utils.Lists;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class ListRemove {
+
+    public static void main(String[] args) {
+        List<String> strList2 = new ArrayList<>();
+
+        strList2.add("a");
+        strList2.add("ab");
+        strList2.add("ac");
+        strList2.add("ad");
+
+        //jdk8
+        strList2.removeIf(s -> (s.equals("a")));
+        System.out.println("结果" + strList2);
+
+
+        List<Integer> list = Lists.newArrayList();
+        list.add(1);
+        list.add(2);
+        list.add(2);
+        list.add(4);
+        //iterator
+        Iterator<Integer> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            Integer integer = iterator.next();
+            if (integer == 2) {
+                iterator.remove();
+            }
+        }
+        System.out.println("结果" + list);
+
+    }
+
+
+}
